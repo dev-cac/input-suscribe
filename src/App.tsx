c786e72641206d50a0a2c787c7ed710e3583b490
LuisFOsG <luisfosgdev@gmail.com>
2022-08-10 00:32:38 -0500

    feat: make dino

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
+import Dino from './components/Dino';
 
-import './App.css'
+import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Dino />
       <div className="Container">
         <label className="Label" htmlFor="name">Suscribete</label>
         <input className='Input' id='name' name="name" type="text" placeholder='Ingrese su nombre *' />
---
+++ src/components/Dino.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useRef } from 'react';
+
+import '../styles/Dino.css';
+
+let sueloX = 0;
+let deltaTime: number | Date = 0;
+let time = new Date();
+
+const velEscenario = 1280/3;
+const gameVel = 1;
+
+function Dino() {
+  const wall = useRef<null | HTMLParagraphElement>(null);
+  const canvasRef = useRef<null | HTMLParagraphElement>(null);
+
+  const calcularDesplazamiento = () => {
+    return velEscenario * +deltaTime * gameVel;
+  }
+
+  useEffect(() => {
+    const MoverSuelo = () => {
+      sueloX += calcularDesplazamiento();
+
+      if (canvasRef.current && wall.current) {
+        canvasRef.current.style.left = -(sueloX % wall.current.clientWidth) + "px";
+      }
+    }
+
+    const Loop = () => {
+      deltaTime = (+new Date() - +time) / 1000;
+      time = new Date();
+      MoverSuelo();
+    }
+
+    function Init() {
+      Loop();
+    }
+
+    const timeOut =  setInterval(Init, 1);
+
+    return () => {
+      clearInterval(timeOut);
+    }
+  }, [])
+
+  return (
+    <div ref={wall} className="ContainerDino">
+      <div ref={canvasRef} className="suelo"></div>
+      <div className="dino dinoCorriendo"></div>
+    </div>
+  )
+}
+
+export default Dino;
